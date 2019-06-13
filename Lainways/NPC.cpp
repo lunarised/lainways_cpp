4c16367bd7014807394e4692d49928a75fdd3057
lunarised <j@p7.co.nz>
2019-06-13 23:25:14 +1200

    Added Flee NPC mechanic

--- Lainways/NPC.cpp
+++ Lainways/NPC.cpp
@@ -22,7 +22,7 @@ NPC::NPC(Graphics^ _canvas, Bitmap^ _spriteSheet, int _frameWidth, int _frameHei
 	}
 }
 void NPC::genState(int _x, int _y) {
-	if (xPos < _x + 128 && xPos > _x - 128 && yPos < _y + 128 && yPos > _y - 128) {
+	if (xPos < _x + 256 && xPos > _x - 256 && yPos < _y + 256 && yPos > _y - 256) {
 		if (health > 2) {
 			state = 1;//Aggro
 		}
@@ -39,7 +39,7 @@ int NPC::action(int _x, int _y) {
 	if (state == 0 || state == 2) {
 		health += (health < 6) ? rand() % 3 : 0;
 	}
-	else if (state == 1) {
+	if (state == 1) {
 		int xD, yD = 0;
 		int xDir, yDir = 0;
 		if (xPos - _x > 0) {
@@ -88,6 +88,7 @@ int NPC::action(int _x, int _y) {
 			if (_x  == xPos - 32 && _y == yPos ) {
 				return rand() % 4;
 			}
+			
 
 			else {
 				xPos -= 32;
@@ -102,6 +103,71 @@ int NPC::action(int _x, int _y) {
 			}
 		}
 	}
+	if (state == 2) {
+		int xD, yD = 0;
+		int xDir, yDir = 0;
+		if (xPos - _x > 0) {
+			xD = Math::Abs(xPos - _x);
+			xDir = 1;
+			//face left
+		}
+		else if (xPos - _x < 0) {
+			//face right
+			xD = Math::Abs(xPos - _x);
+			xDir = 2;
+		}
+		if (yPos - _y > 0) {
+			//face up
+			yD = Math::Abs(yPos - _y);
+			yDir = 0;
+		}
+		else if (yPos - _y < 0) {
+			yD = Math::Abs(yPos - _y);
+			yDir = 3;
+			//facedown
+		}
+		if (xD > yD) {
+			direction = xDir;
+		}
+		else {
+			direction = yDir;
+		}
+		if (Math::Abs(tm->GetMapEntry((xPos / 32), (yPos / 32) + 1)) >= 2 && direction == 0) {
+			if (_x == xPos && _y == yPos + 32) {
+				return rand() % 4;
+			}
+			else {
+				yPos -= 32;
+			}
+		}
+		else if (Math::Abs(tm->GetMapEntry((xPos / 32) + 1, (yPos / 32))) >= 2 && direction == 1) {
+			if (_x == xPos + 32 && _y == yPos) {
+				return rand() % 4;
+			}
+			else {
+				xPos -= 32;
+			}
+		}
+		else if (Math::Abs(tm->GetMapEntry((xPos / 32) - 1, (yPos / 32))) >= 2 && direction == 2) {
+			if (_x == xPos - 32 && _y == yPos) {
+				return rand() % 4;
+			}
+
+
+			else {
+				xPos += 32;
+			}
+		}
+		else if (Math::Abs(tm->GetMapEntry((xPos / 32), (yPos / 32) - 1)) >= 2 && direction == 3) {
+			if (_x == xPos && _y == yPos - 32) {
+				return rand() % 4;
+			}
+			else {
+				yPos += 32;
+			}
+		}
+	}
+
 }
 void NPC::draw(int _xVP, int _yVP) {
 	if (xPos > _xVP - frameWidth && yPos > _yVP - frameHeight && xPos < _xVP + 800 && yPos < _yVP + 600) {
@@ -109,9 +175,6 @@ void NPC::draw(int _xVP, int _yVP) {
 		canvas->DrawImage(spriteSheet, (xPos - _xVP) , yPos - _yVP , pixelsToDraw, GraphicsUnit::Pixel);
 	}
 }
-
-
-
 
 void NPC::updateFrame() {
 	currentFrame = (currentFrame + 1) % nFrames;
